852057fb6e226911bcad2e98cc907255ae84daad
Martin <martinandrovich@gmail.com>
2021-11-06 19:20:23 +0100

    update demo

--- wsg50/examples/demo_wsg50.cpp
+++ wsg50/examples/demo_wsg50.cpp
@@ -6,6 +6,9 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "demo_wsg50");
 	ros::NodeHandle nh;
 	
+	// first run:
+	// roslaunch ur5_gazebo ur5.launch ee:=wsg50
+	
 	// grasp while blocking
 	wsg50::grasp(true);
 	wsg50::release(true);
